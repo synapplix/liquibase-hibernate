89c0540a6afebd1a4db9c8f37388a74405427dd5
Robert Rettig <rrettig@synapplix.de>
2014-08-07 18:41:29 +0200

    - bypass the 15 character limit on primary key names, extend it and adding a suffix with hashcode of hibernate table name

--- src/main/java/liquibase/ext/hibernate/snapshot/PrimaryKeySnapshotGenerator.java
+++ src/main/java/liquibase/ext/hibernate/snapshot/PrimaryKeySnapshotGenerator.java
@@ -8,7 +8,17 @@ import liquibase.structure.core.Index;
 import liquibase.structure.core.PrimaryKey;
 import liquibase.structure.core.Table;
 
+import org.hibernate.sql.Alias;
+
 public class PrimaryKeySnapshotGenerator extends HibernateSnapshotGenerator {
+
+    private static final int PKNAMELENGTH = 30;
+    private static final String PK = "PK";
+    /**
+     * This is the same Alias as in org.hibernate.mapping.PersistentClass.PK_ALIAS
+     */
+    private static final Alias PK_ALIAS_15 = new Alias(15, PK);
+    private static final Alias NEW_PK_ALIAS = new Alias(PKNAMELENGTH, PK);
 
     public PrimaryKeySnapshotGenerator() {
         super(PrimaryKey.class, new Class[]{Table.class});
@@ -30,7 +40,28 @@ public class PrimaryKeySnapshotGenerator extends HibernateSnapshotGenerator {
             org.hibernate.mapping.PrimaryKey hibernatePrimaryKey = hibernateTable.getPrimaryKey();
             if (hibernatePrimaryKey != null) {
                 PrimaryKey pk = new PrimaryKey();
-                pk.setName(hibernatePrimaryKey.getName());
+                String hbnPrimaryKeyName = hibernatePrimaryKey.getName();
+                /*
+                 * pk name is probably truncated and maybe a duplicate in case
+                 * of tables with long prefixes
+                 */
+                String hbnTableName = hibernateTable.getName();
+                if (hbnPrimaryKeyName.length() == 15
+                        && hbnPrimaryKeyName.equals(PK_ALIAS_15.toAliasString(hbnTableName))) {
+                    LOG.warning("Hibernate primary key name is probably truncated. " + hbnPrimaryKeyName);
+                    String newAlias = NEW_PK_ALIAS.toAliasString(hbnTableName);
+                    int newAliasLength = newAlias.length();
+                    if (newAliasLength > 15) {
+                        if (newAliasLength == PKNAMELENGTH) {
+                            String suffix = "_" + Integer.toHexString(hbnTableName.hashCode()).toUpperCase() + "_" + PK;
+                            hbnPrimaryKeyName = newAlias.substring(0, PKNAMELENGTH - suffix.length()) + suffix;
+                        } else {
+                            hbnPrimaryKeyName = newAlias;
+                        }
+                        LOG.warning("Changing hibernate primary key name to " + hbnPrimaryKeyName);
+                    }
+                }
+                pk.setName(hbnPrimaryKeyName);
                 pk.setTable(table);
                 for (Object hibernateColumn : hibernatePrimaryKey.getColumns()) {
                     pk.getColumnNamesAsList().add(((org.hibernate.mapping.Column) hibernateColumn).getName());
